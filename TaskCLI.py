6249c7e45349610b4b3eae887990632b5a852d39
adamfek <adamfesama@gmail.com>
2024-10-12 01:22:05 +0100

    implement Task manager class and some methods

---
+++ TaskCLI.py
@@ -0,0 +1,32 @@
+import os
+import json
+from datetime import datetime
+class TaskManager:
+    def __init__(self,Taskid,Desc,status):
+        self.Taskid = Taskid
+        self.Desc =Desc
+        self.createdAt=datetime.now()
+        self.updatedAt = datetime.now()
+        self.tasks = []  # List to store tasks
+     
+    def Update_Task(self,newTaskid,newdesc):
+        self.Taskid=newTaskid
+        self.desc=newdesc
+        self.updatedAt=datetime.now()
+
+    def add_Task(self,Desc,status):
+        self.Taskid += 1
+        self.Desc =Desc
+        self.createdAt=datetime.now()
+        self.updatedAt = datetime.now()
+        new_task = TaskManager(self.Taskid,self.desc)
+        self.tasks.append(new_task)
+    
+    def delet_Task(self,Taskid):
+        if not any(task.Taskid == Taskid for task in self.tasks):
+            print("This task does not exist, sir!")
+        else:
+            self.tasks.remove(task)
+    
+    def update_status(self,status):
+        
